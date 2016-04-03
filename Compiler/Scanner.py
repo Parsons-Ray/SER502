a08ca2836083ff324383c800c5150625ca619251
shashanklmur <shashanklmur@gmail.com>
2016-04-03 13:11:10 -0700

    Changes to scanner file - extended grammar for declarativeStatement

--- Compiler/Scanner.py
+++ Compiler/Scanner.py
@@ -8,21 +8,48 @@ import pyparsing as pp
 # numeric = pp.Word(pp.nums)
 # endChar = pp.Literal(".")
 #
-# declaration = typeName + identifier + pp.Optional(  assignment + numeric) + pp.ZeroOrMore(pp.Literal(",") + identifier + pp.Optional( assignment + numeric )) + endChar
+# declaration = typeName + identifier + pp.Optional( assignment + numeric) + pp.ZeroOrMore(pp.Literal(",") + identifier + pp.Optional( assignment + numeric )) + endChar
 #
 #
 #
 #
 #
 # print declaration.parseString("integer a := 10, \n b := 15, c:=20, d.")
+#
 
-# functionCallStatement
-# assignmentStatement = pp.Or(identifier ^ indentifier + lsqBracs + numericLiteral + rsqBracs) + assign + expr + eol
-# nullStatement = pp.Literal("none")
+
+eol = pp.Literal(".")
+assign = pp.Literal(":=")
+lsqBracs = pp.Literal("[")
+rsqBracs = pp.Literal("]")
+numericLiteral = pp.Optional(pp.nums) + pp.Optional(pp.Literal(".") + pp.nums)
+stringLiteral = pp.Word(pp.alphas, pp.alphanums)
+identifier = stringLiteral
+size = numericLiteral
+unaryAddingOperator = pp.Or(pp.Literal("+") ^ pp.Literal("-"))
+binaryAddingOperator = pp.Or(pp.Literal("+") ^ pp.Literal("-"))
+multiplyingOperator = pp.Or(pp.Literal("*") ^ pp.Literal("/"))
+relationalOperator = pp.Or(pp.Literal("==") ^ pp.Literal("!=") ^ pp.Literal( "<" ) ^ pp.Literal(">") ^ pp.Literal("<=") ^ pp.Literal(">="))
+indexedComponent = pp.Literal("[") + (numericLiteral + pp.ZeroOrMore("," + numericLiteral) )+ pp.Literal("]")
+name = pp.Or(identifier ^ indexedComponent)
+primary = pp.Or(numericLiteral ^ name  ^ pp.Literal("true") ^ pp.Literal("false"))
+factor = pp.Or((primary + pp.Optional(pp.Literal("^") + primary)) ^ (pp.Literal("NOT") + primary))
+term = factor + pp.ZeroOrMore(multiplyingOperator + factor)
+simpleExpression = pp.Optional(unaryAddingOperator) + term + pp.ZeroOrMore(binaryAddingOperator + term)
+relation = simpleExpression + pp.ZeroOrMore(pp.Or(pp.Literal(",") + simpleExpression) ^ (relationalOperator + simpleExpression))
+expr = relation + pp.ZeroOrMore(pp.Or((pp.Literal("AND") + relation) ^ (pp.Literal("OR") + relation)))
+printStatement = pp.Literal("print") + expr
+returnStatement = pp.Literal("return") + expr
+typeName = pp.Or(pp.Literal("integer") ^ pp.Literal("floating") ^ pp.Literal("boolean"))
+ArrayTypeDefinition = pp.Literal("Array") + typeName + pp.Literal("[") + (size + pp.ZeroOrMore(pp.Literal(",") + size))+ pp.Literal("]")
+typeDefinition = pp.Or(typeName ^ ArrayTypeDefinition)
+declarativeStatement = typeDefinition + identifier + pp.Optional( assign + expr) + pp.ZeroOrMore(pp.Literal(",") + identifier + pp.Optional( assign + expr)) + eol
+# functionCallStatement =
+assignmentStatement = identifier + pp.Optional(lsqBracs + numericLiteral + rsqBracs) + assign + expr + eol
+nullStatement = pp.Literal("none")
 # functionSpecification =
-# compoundStatement = pp.Or(ifStatement ^ loopStatement)
-# simpleStatement = pp.Or(nullStatement  ^ assignmentStatement ^ functionCallStatement ^ declarativeStatement ^ printStatement ^ returnStatement)
-# statement = pp.Or(simpleStatement ^ compoundStatement ^ functionSpecification)
-# sequenceOfStatements = pp.OneOrMore(statement)
-# program = sequenceOfStatements
-
+compoundStatement = pp.Or(ifStatement ^ loopStatement)
+simpleStatement = pp.Or(nullStatement  ^ assignmentStatement ^ functionCallStatement ^ declarativeStatement ^ printStatement ^ returnStatement)
+statement = pp.Or(simpleStatement ^ compoundStatement ^ functionSpecification)
+sequenceOfStatements = pp.OneOrMore(statement)
+program = sequenceOfStatements
