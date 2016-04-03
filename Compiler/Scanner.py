0431e2bbb63d6528e3a27749964d4aece8becd8b
Digant Jagtap <digantjagtap@utoadmins-Macintosh.local>
2016-04-03 15:45:42 -0700

    Compound Statement Changes

--- Compiler/Scanner.py
+++ Compiler/Scanner.py
@@ -22,12 +22,26 @@ eol = pp.Literal(".")
 assign = pp.Literal(":=")
 lsqBracs = pp.Literal("[")
 rsqBracs = pp.Literal("]")
-numericLiteral = pp.Optional(pp.nums) + pp.Optional(pp.Literal(".") + pp.nums)
+numeric = pp.Word(pp.nums)
+plus = pp.Literal("+")
+minus = pp.Literal("-")
+when = pp.Keyword("when")
+elsevar = pp.Keyword("else")
+elseWhen = pp.Keyword("elseWhen")
+loop = pp.Keyword("loop")
+lBracs = pp.Literal("(")
+rBracs = pp.Literal(")")
+lcrBracs = pp.Literal("{")
+rcrBracs = pp.Literal("}")
+andExpr = pp.Keyword("AND")
+orExpr = pp.Keyword("OR")
+
+numericLiteral = pp.Optional(numeric) + pp.Optional(eol + numeric)
 stringLiteral = pp.Word(pp.alphas, pp.alphanums)
 identifier = stringLiteral
 size = numericLiteral
-unaryAddingOperator = pp.Or(pp.Literal("+") ^ pp.Literal("-"))
-binaryAddingOperator = pp.Or(pp.Literal("+") ^ pp.Literal("-"))
+unaryAddingOperator = pp.Or(plus ^ minus)
+binaryAddingOperator = pp.Or(plus ^ minus)
 multiplyingOperator = pp.Or(pp.Literal("*") ^ pp.Literal("/"))
 relationalOperator = pp.Or(pp.Literal("==") ^ pp.Literal("!=") ^ pp.Literal( "<" ) ^ pp.Literal(">") ^ pp.Literal("<=") ^ pp.Literal(">="))
 indexedComponent = pp.Literal("[") + (numericLiteral + pp.ZeroOrMore("," + numericLiteral) )+ pp.Literal("]")
@@ -47,7 +61,13 @@ declarativeStatement = typeDefinition + identifier + pp.Optional( assign + expr)
 # functionCallStatement =
 assignmentStatement = identifier + pp.Optional(lsqBracs + numericLiteral + rsqBracs) + assign + expr + eol
 nullStatement = pp.Literal("none")
-# functionSpecification =
+# compoundStatement =
+# DOUBT : expression = pp.Optional(lsqBracs) +  + pp.Optional(rcrBracs) + pp.Or(pp.ZeroOrMore(andExpr + relation) ^ pp.ZeroOrMore(orExpr + relation))
+# Changed expression from relation ({ "AND" relation } | { "OR" relation }) | "[" relation "]" to ["["] + relation + ["]"] {"AND" relation } | { "OR" relation }
+condition = expr
+# DOUBT : how to refer sequenceOfStatements if defined below?
+loopStatement = loop + lBracs + condition + rBracs + lcrBracs + sequenceOfStatements + rcrBracs
+ifStatement = when + lBracs + condition + rBracs + lcrBracs + sequenceOfStatements + rcrBracs + pp.ZeroOrMore(elseWhen + lBracs + condition + rBracs + lcrBracs + sequenceOfStatements + rcrBracs) + pp.Optional(elsevar + lcrBracs + sequenceOfStatements + rcrBracs)
 compoundStatement = pp.Or(ifStatement ^ loopStatement)
 simpleStatement = pp.Or(nullStatement  ^ assignmentStatement ^ functionCallStatement ^ declarativeStatement ^ printStatement ^ returnStatement)
 statement = pp.Or(simpleStatement ^ compoundStatement ^ functionSpecification)
