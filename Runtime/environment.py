7f37538af7d15a237dd27bc102701ca52f3f55c8
Kevin <kevin@kevins-mbp.mobile.asu.edu>
2016-04-24 01:31:51 -0700

    Function is working

--- Runtime/environment.py
+++ Runtime/environment.py
@@ -4,10 +4,9 @@ import re
 #Globals
 #highlevel code looks like:
     #integer a, b := 10, c:=20.
-#tokens = Iterator(["TYP", "INT", "a", "a", "EQL", "NULL", "TYP", "INT", "b", "b", "EQL", "10", "TYP", "INT", "c", "c", "EQL", "20", "EOL"])
 
 current_scope = "GLBL"
-tokens = Iterator(["FUN", "sampleFunction", "INT", "PAR", "INT", "param1", "PAR", "BOOL", "param2", "STRT", "TYP", "INT", "a", "a", "EQL", "10", "EOL", "END"])
+tokens = Iterator(["FUN", "sampleFunction", "INT", "PAR", "INT", "param1", "PAR", "INT", "param2", "STRT", "TYP", "INT", "a", "STRTEX", "PUSH", "10", "EQL", "a", "ENDEX", "FUNEND", "CALL", "sampleFunction", "PAR", "10", "PAR", "20", "SDK"])
 
 glbl_labeldict = {} #this contains all the labels and lines inside them
 
@@ -16,16 +15,11 @@ loop_labeltokens = Iterator([".LABEL2", "LOOP", "STRTEX", "PUSH", "a", "PUSH", "
 stack = Stack()
 
 def TYP():
-    #create varaible
-    while (tokens.next() is not "EOL"): #this should also pop off "TYP" along with "EOL"
-        varType = tokens.next() #pushes "INT, FLT, BOOL" onto stack
-        varID = tokens.next() #variable identifier ("a", "b", "c")
-        if tokens.next() is "EQL":#remove "EQL"
-            varVal = tokens.next()
-            var = Variable(varID, varVal, varType)
-        else:
-            var = Variable(varID, "NULL", varType)
-        dict_of_symbolTabs[current_scope].add(varID, var)
+    tokens.next() #this should pop off "TYP"
+    varType = tokens.next() #pushes "INT, FLT, BOOL" onto stack
+    varID = tokens.next() #variable identifier ("a", "b", "c")
+    var = Variable(varID, None, varType)
+    dict_of_symbolTabs[current_scope].add(varID, var)
 
 def FUN():
     tokens.next()#pop FUN
@@ -33,17 +27,32 @@ def FUN():
     currentFunction = Function(tokens.next(), name) #return type
     while (tokens.next() is "PAR"):
         currentFunction.addParam(tokens.next(), tokens.next())
-    tokens.next() #pop off "STRT"
     currentFunction.startPC = tokens.counter
-    dict_of_symbolTabs[current_scope].add(name, currentFunction) #ideally this will be 'current_scope' = "global"
+    dict_of_functions[name] = currentFunction
+    while tokens.current() is not "FUNEND":
+        print(tokens.next())
+    tokens.next()#pop End
 
 def CALL():
     tokens.next() #pop CALL
-    currentFunction = dict_of_symbolTabs[current_scope].lookup(tokens.next())
-    while tokens.next() is "PAR":
+    currentFunction = dict_of_functions[tokens.next()]#functions should only be declared in global
+    while tokens.next() is "PAR": #add all parameters
         currentFunction.setParamValues(tokens.next())
-    currentFunction.returnPC(tokens.current())
-    currentFunction.setSymbolTable(current_scope)
+    currentFunction.returnPC(tokens.getCounter()-1)#set return PC
+    print("returnPC: {0}".format(tokens.getCounter()))
+
+    name = currentFunction.getName()
+    counter = 0
+    while (name + str(counter)) in dict_of_symbolTabs:
+        counter += 1
+
+    global current_scope
+    symbTable = SymbolTable(name, current_scope)
+    for key in currentFunction.getParams():
+        symbTable.add(key, currentFunction.getParams()[key])
+
+    dict_of_symbolTabs[name + str(counter)] = symbTable
+    current_scope = name + str(counter) #set current_scope to the name of the function + number
     stack.push(currentFunction)
     tokens.setCounter(currentFunction.getStartPC())
 
@@ -68,19 +77,117 @@ def LABL(label):
     else:
         print "Label Error: "+ label
 
+def STARTEX():
+    while tokens.current() is not "ENDEX":
+        nextToken = tokens.next()
+        if nextToken == "PUSH":
+            varName = tokens.next()
+            if re.match(r'[+-]?(\d+(\.\d*)?|\.\d+)', varName):
+                stack.push(varName)
+            else:
+                try:
+                    stack.push(dict_of_symbolTabs[current_scope].lookup(varName).getValue())
+                except ValueError:
+                    print("Symbol not found")
+
+        elif nextToken == "ADD":
+            result = stack.pop() + stack.pop()
+            stack.push(result)
+        elif nextToken == "SUB":
+            first = stack.pop()
+            result = stack.pop() - first
+            stack.push(result)
+        elif nextToken == "MUL":
+            result = stack.pop() * stack.pop()
+            stack.push(result)
+        elif nextToken == "DIV":
+            first = stack.pop()
+            result = stack.pop() / first
+            stack.push(result)
+        elif nextToken == "GT":
+            top = stack.pop()
+            if stack.pop() > top:
+                stack.push(1)
+            else:
+                stack.push(0)
+        elif nextToken == "LT":
+            top = stack.pop()
+            if stack.pop() < top:
+                stack.push(1)
+            else:
+                stack.push(0)
+        elif nextToken == "GE":
+            top = stack.pop()
+            if stack.pop() >= top:
+                stack.push(1)
+            else:
+                stack.push(0)
+        elif nextToken == "LE":
+            top = stack.pop()
+            if stack.pop() <= top:
+                stack.push(1)
+            else:
+                stack.push(0)
+        elif nextToken == "EEQL":
+            if stack.pop() == stack.pop():
+                stack.push(1)
+            else:
+                stack.push(0)
+        elif nextToken == "NEQL":
+            if stack.pop() != stack.pop():
+                stack.push(1)
+            else:
+                stack.push(0)
+        elif nextToken == "NOT":
+            if stack.pop() >= 1:
+                stack.push(0)
+            else:
+                stack.push(1)
+        elif nextToken == "EQL":
+            try:
+                varObj = dict_of_symbolTabs[current_scope].lookup(tokens.next())
+                varObj.setValue(stack.pop())
+            except ValueError:
+                print("Identifier not found")
+        elif nextToken == "AND":
+            if stack.pop() and stack.pop():
+                stack.push(1)
+            else:
+                stack.push(0)
+        elif nextToken == "OR":
+            if stack.pop() or stack.pop():
+                stack.push(1)
+            else:
+                stack.push(0)
+    tokens.next()
+
+def FUNEND():
+    finishedFunction = stack.pop()
+    print("here: {0}".format(finishedFunction.getRetrunPC()))
+    tokens.setCounter(finishedFunction.getRetrunPC())
 
 def main():
-    current_scope = "global"
-    nextToken = tokens.current()
-    if nextToken is "TYP":
-        TYP()
-    elif nextToken is "FUN":
-        FUN()
-    elif nextToken is "CALL":
-        CALL()
-    elif nextToken is "RTRN":
-        RTRN()
+    dict_of_symbolTabs[current_scope] = GlblSymTab()
+    while tokens.current() is not "SDK":
+        print("dict_of_symbolTabs: {0}".format(dict_of_symbolTabs))
+        nextToken = tokens.current()
+        print(nextToken)
+        if nextToken is "TYP":
+            TYP()
+        elif nextToken is "FUN":
+            FUN()
+        elif nextToken is "CALL":
+            CALL()
+        elif nextToken is "RTRN":
+            RTRN()
+        elif nextToken is "STRTEX":
+            STARTEX()
+        elif nextToken is "FUNEND":
+            FUNEND()
+        else:
+            tokens.next()
 
-    LABL(curr_labeltokens.current())
+    #LABL(curr_labeltokens.current())
+
 #Call the main method. Starts runtime.
 main()
